5b4f76cd4ca389c6ac4b47261cc118a65d599ef5
dkonuk <dkonuk@gmail.com>
2024-10-22 23:51:20 +0300

    Added new tests

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -9,20 +9,20 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 
-@pytest.fixture(params=["chrome"])
+@pytest.fixture(params=["chrome", "firefox"])
 def fixtureSetup(request):
     driver = None
     if request.param == "chrome":
         chrome_options = ChromeOptions()
-        #chrome_options.add_argument("-headless=new")
+        chrome_options.add_argument("-headless=new")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument("disable-blink-features=AutomationControlled")
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
-    # if request.param == "firefox":
-    #     firefox_options = FirefoxOptions()
-    #     firefox_options.add_argument("-headless")
-    #     firefox_options.add_argument("-no-sandbox")
-    #     firefox_options.add_argument("-window-size=1920,1080")
-    #     driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=firefox_options)
+    if request.param == "firefox":
+        firefox_options = FirefoxOptions()
+        firefox_options.add_argument("-headless")
+        firefox_options.add_argument("-no-sandbox")
+        firefox_options.add_argument("-window-size=1920,1080")
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=firefox_options)
     return driver
